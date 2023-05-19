72b90a852ff7dc4ac9e5ca9d04dc7f149cfac596
JooYoung2274 <jooooo2274@gmail.com>
2023-05-19 11:58:55 +0900

    MUD_GAME DI 프레임워크로 마이그레이션

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import express from "express";
+import helmet from "helmet";
+import "dotenv/config";
+import cors from "cors";
+import { registerRoutes, userController } from "./router/test.router";
+
+const app = express();
+app.use(helmet());
+app.use(express.json());
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+registerRoutes(app, userController);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
